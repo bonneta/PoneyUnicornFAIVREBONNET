d976f7e9a0e555930a805c55bf9b64450c81e739
Anais <anais@aze.com>
2017-03-17 17:01:06 +0100

    fourth commit

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
  * Created by ab on 3/17/17.
  */
 
+let deadpool = new Deadpool ();
+let fluttershy = new Poney ();
+let twilightSparkles = new Poney ();
+
+
+setInterval(() => {
+  setInterval(() => {
+
+
+  }, 50000);
+}, 100000);
